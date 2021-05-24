3e2b92ba8017495ebcdc6b9210f29d3ca3781764
wolf40 <wolf40@hm.edu>
2021-05-24 11:33:09 +0200

    Database connection: Entity Framework tables

---
+++ src/SEIIApp/Server/DataAccess/DatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Server.DataAccess
+{
+    public class DatabaseContext : DbContext
+    {
+         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+
+
+        public DbSet<Domain.User> User { get; set; }
+
+        public DbSet<Domain.SubjectArea> SubjectAreas { get; set; }
+
+        public DbSet<Domain.Lecture> Lectures { get; set; }
+
+        public DbSet<Domain.Test> Tests { get; set; }
+
+        //public DbSet<Domain.CompletedTest> CompletedTests { get; set; }
+
+        public DbSet<Domain.News> News{ get; set; }
+
+    }
+}
