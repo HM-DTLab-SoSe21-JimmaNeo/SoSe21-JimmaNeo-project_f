786d60b0b4a17281b53cf67d6cfbfa0d7693e65f
CURRY\Hoang <le.hoanglong@hotmail.de>
2021-06-14 20:06:12 +0200

    optimized News and finished the layout. Added new attributes to DTOS and Services. #49 and # 47

--- src/SEIIApp/Server/Services/TestService.cs
+++ src/SEIIApp/Server/Services/TestService.cs
@@ -66,10 +66,13 @@ namespace SEIIApp.Server.Services
             DatabaseContext.Tests.Add(test);
             DatabaseContext.SaveChanges();
 
-            NewsService.AddNews(new News() { 
-                Topic = "New Test", 
+            NewsService.AddNews(new News()
+            {
+                Topic = "New Test",
                 Content = $"A new Test, named {test.Topic}, was uploaded to this platform. The Test was created by {test.Author.Name}.",
-                DateOfCreation = DateTime.Now
+                DateOfCreation = DateTime.Now,
+                Creator = "System",
+                Tags = "New ,Exam, Challenge, Power, FUN"
             });
 
             return test;
@@ -87,7 +90,9 @@ namespace SEIIApp.Server.Services
             {
                 Topic = $"Updated Test {test.Topic}",
                 Content = $"The Test \"{test.Topic}\" has been updated. The Test was updated by {test.Author.Name}.",
-                DateOfCreation = DateTime.Now
+                DateOfCreation = DateTime.Now,
+                 Creator = "System",
+                Tags = "Update, Exam, Challenge, Power, FUN"
             });
 
             return exsistingTest;
