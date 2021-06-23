21162caee023bd48edb62185fcca9ace66aa7484
hm-maghy <hm-maghy@hm.edu>
2021-06-23 11:34:20 +0200

    Neuer Service um die DataItems für die Diagramme für Welcome Student zu generieren

---
+++ src/SEIIApp/Client/Services/DataStudentItemService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEIIApp.Client.Services
+{
+    public class DataStudentItemService
+    {
+        public string TestName { get; set; }
+        public double ReachedPoints { get; set; }
+
+        public double maxPointsfromTests { get; set; }
+        public double maxReachedPointsfromTests { get; set; }
+    }
+}
