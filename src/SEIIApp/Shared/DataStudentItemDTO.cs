b79455bd3fccb36e0b77bac805e285cbe29001f5
hm-maghy <hm-maghy@hm.edu>
2021-06-23 11:35:22 +0200

    ZUgehörige DTO für die DataItems zum Erstellen der Diagramme für Student

---
+++ src/SEIIApp/Shared/DataStudentItemDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEIIApp.Shared
+{
+    class DataStudentItemDTO
+    {
+        public string TestName { get; set; }
+        public double ReachedPoints { get; set; }
+    }
+}
