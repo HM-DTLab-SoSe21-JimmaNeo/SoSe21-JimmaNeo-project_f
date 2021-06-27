070e2b2216ef44dce933c01c1efd03a9b93720b6
Anne Sofie Weber <annesofieweber@MacBook-Air-von-Anne.local>
2021-06-27 20:16:18 +0200

    statische Variable in PageRunTest entfernt

--- src/SEIIApp/Shared/CompletedTestDTO.cs
+++ src/SEIIApp/Shared/CompletedTestDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,8 @@ namespace SEIIApp.Shared
   public class CompletedTestDTO
   {
     public int CtId { get; set; }
-
-    public TestBaseDTO SolvedTest { get; set; }
+    [Required]
+    public TestDTO SolvedTest { get; set; }
 
     public UserDTO Student { get; set; }
 
